98e65f5495fd1f53a06ba787c210dd5325e8f60a
evan <aspaasevan@gmail.com>
2020-05-08 08:43:25 -0700

    api/projects returns all projects" "

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -4,4 +4,16 @@ const projectDB = require('../data/helpers/projectModel.js');
 const router = express.Router();
 
 
+// Return all users from db
+router.get('/', (req, res) => {
+  projectDB.get()
+    .then(projects => {
+      res.status(200).json(projects)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ errorMessage: `Error retrieving lists of projects`});
+    });
+});
+
 module.exports = router;
